a494afa7a7cfcd41601a8af00b40b9d2b9576141
MR ARFAN <46324706+muhdarfan@users.noreply.github.com>
2021-06-11 00:29:58 +0800

    Update config for production

--- Library/config.php
+++ Library/config.php
@@ -9,3 +9,13 @@ $Config = array(
     )
 );
 */
+
+// Heroku
+$Config = array(
+    'mysql' => array(
+        'host' => 'us-cdbr-east-04.cleardb.com',
+        'student' => 'b0f4383aad317e',
+        'pass' => 'f76202a1',
+        'db' => 'heroku_b3e6f981c80d4cd',
+    )
+);
